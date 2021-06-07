cf9aa4e9bf2bfdab1c19c66fde07cd552f800011
Harshil Khamar <73790584+Harshil0512@users.noreply.github.com>
2021-06-07 13:10:35 +0530

    Add files via upload

---
+++ src/Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript</h1>
+      <p>
+        JavaScript (often shortened to JS) is a lightweight, interpreted,
+        object-oriented language with first-class functions, and is best known
+        as the scripting language for Web pages, but it's used in many
+        non-browser environments as well. ... JavaScript can function as both a
+        procedural and an object oriented language.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+
+var t = document.getElementById("root");
+
+ReactDom.render(
+  <div>
+    <App />
+  </div>,
+  t
+);
